20bc1870bab4628f5470b8aed37a538e076e3b78
No0910 <nestea_93@hotmail.fr>
2022-10-26 00:22:26 +0200

    Page produit faite, mais manque ajout au panier avec localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,20 +1,52 @@
-// Afficher le produit //
+//// Afficher le produit ////
 
 
 
-//Récupération de l'id dynamique de l'url//
+// 1 : Récupérer de l'id dynamique de l'url
 
-let link ="http://localhost:3000/api/products/product.html?id="
+let urlParam = (new URL(location)).searchParams
+let productId = urlParam.get("id")
 
-let url= (new URL(link));
-
-let id = url.searchParams.get("id");
-
-console.log(id);
-
-// Appel de l'API pour un seul produit//
+console.log(productId);
 
 
+// 2 : Récupérer les élements pour injecter les infos du produit
 
+let productTitle = document.getElementById("title");
+let productColor = document.getElementById("colors");
+let productImage = document.querySelector(".item__img");
+let productDescription = document.getElementById("description");
+let productPrice = document.getElementById("price");
+
+
+
+// 3 : Appeler l'API pour récupérer les informations du produit à afficher
+
+fetch("http://localhost:3000/api/products/" + productId)
+    .then((response) => {
+        if (response.ok) {
+            response.json()
+
+                .then((product) => {
+                   
+                    // Injecter les informations dans le HTML
+                    productImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+                    productTitle.innerHTML = `${product.name}`
+                    productPrice.innerHTML = `${product.price}`
+                    productDescription.innerHTML = `${product.description}`
+                    let colorArray = product.colors
+                    for (let color of colorArray) {
+                        productColor.innerHTML += `<option value="${color}"> ${color}</option>`
+                    }
+
+                })
+                .catch((error) => {
+                    alert("Erreur: Le produit n'est pas disponible")
+                })
+        }
+
+    }).catch((error) => {
+    alert("Pas de réponse du serveur")
+})
 
 
--- front/js/script.js
+++ front/js/script.js
@@ -34,18 +34,6 @@ fetch("http://localhost:3000/api/products/")
                         baliseArticle.appendChild(baliseH3);
                         baliseArticle.appendChild(baliseP);
 
-                       
-
-
-                        // Puis injecter les informations du produit dans le HTML
-        /*                items.innerHTML += `<a href="./product.html?id=${product._id}">
-                        <article>
-                          <img src="${product.imageUrl}" alt="${product.altTxt}">
-                          <h3 class="productName">${product.name}</h3>
-                          <p class="productDescription">${product.description}</p>
-                        </article>
-                      </a>`
-*/
                     }
 
                 })
