3797b47281fed48e191521a34a823a05d7a31508
No0910 <nestea_93@hotmail.fr>
2022-11-04 19:56:49 +0100

    Page cart.js en cours

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,109 +1,188 @@
-////////////////////////////////////////////////////////////////
-// On affiche tout les produits dans le localstorage ////////////////////
-////////////////////////////////////////////////////////////////
+
+// 1. Je récupère le contenu de mon panier
+
+let basket = JSON.parse(localStorage.getItem("products"));
+
+if ( basket == null || basket.length <= 0 ) {
+
+document.getElementById('totalQuantity').innerHTML = "0";
+
+let baliseNoItems = document.createElement("p");
+baliseNoItems.textContent = "Pas d'articles";
+
+document.getElementById('cart__items').appendChild(baliseNoItems);
 
 
-// récupérer le panier (l’array) via localStorage.
-let productInLocalStorage = (localStorage.getItem("products"));
-console.log(productInLocalStorage);
+} else {
 
-let cartTotalPrice = 0 ;
-let cartTotalItems = 0 ;
+let nbProducts = basket.length;
 
+document.getElementById('totalQuantity').innerHTML = nbProducts;
 
-function getProductOfCart() {
-  //Object.entries permets de transformer un objet en tableau
-  //la première boucle va au premier niveau de l'objet
-  for (let [id, colors] of Object.entries(productInLocalStorage)) {
-    //la deuxième boucle va au deuxième niveau
-    //On récupère la couleur et la quantité
-    for (let [color, quantity] of Object.entries(colors)) {
-      fetch("http://localhost:3000/api/products/" + id)
-        .then((response) => {
-          if (response.ok) {
-            response.json()
-              .then((productData) => {
+  // 2. J'affiche un tableau récapitulatif des produits / Je parcours mon localStorage
 
-                let item = document.querySelector("#cart__items")
-                // Insertion des éléments
-                item.innerHTML += `<article class="cart__item" data-id ="${productData._id}" data-color="${color}">
-                <div class="cart__item__img">
-                  <img src="${productData.imageUrl}" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${productData.name}</h2>
-                    <p>${color}</p>
-                    <p>${productData.price} €</p> 
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté :   </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-                </article>`
-                //Supprimer un produit du ls
-                //deleteBtns renvoie un objet avec tous les éléments html sous forme de collection
-                let deleteBtns = document.getElementsByClassName("deleteItem")
+  for (let i = 0; i < nbProducts; i++) {
+ 
+// Je récupère le produit courant
 
-                //le forEach permets de boucler sur les boutons récupérés dans deleteBtns
-                //Object.values nous permet d'établir un tableau pour pouvoir boucler sur les éléments
-                Object.values(deleteBtns).forEach(deleteBtn => {
-                  // Au clic de deleteBtn on supprime le produit du ls
-                  deleteBtn.addEventListener('click', function () {
-                    //closest permet de récupérer l'article le plus proche du deleteBtn en question
-                    let article = deleteBtn.closest("article")
-                    let deleteBtnId = article.getAttribute("data-id")
-                    let deleteBtncolor = article.getAttribute("data-color")
+let product = basket[i];
 
-                    //Fonction récupérer dans le fichier localstorage, qui permet de supprimer le produit du ls
-                    removeProductFromLs(deleteBtnId, deleteBtncolor)
+console.log( product );
 
-                  })
-                })
+// 3. Pour chaque produit, je dois créer une ligne dans mon tableau ( qui correspond à ce qui est commenté dans cart.html )
 
-                // Changer la quantité d'un produit dans le ls
-                //quantityItem renvoie un objet avec tous les éléments html sous forme de collection
-                let quantityItem = document.getElementsByClassName("itemQuantity")
+// TODO
+// Je veux créer un article, à l’intérieur de ma section déjà existante
+// Je récupère l’élément « section » qui existe déjà pour y créer mes éléments actuellement commentés dans mon html
 
-                //Object.values nous permet d'établir un tableau pour pouvoir boucler sur les éléments
-                Object.values(quantityItem).forEach(qty => {
-                  qty.addEventListener('change', function () {
-                    let article = qty.closest("article")
-                    let idData = article.getAttribute("data-id")
-                    let idColor = article.getAttribute("data-color")
-                    let newQuantity = qty.value
+let baliseSection = document.getElementById ("cart__items") ;
 
-                    changeProductQuantity(idData, idColor, newQuantity)
-                  })
-                })
+// Puis je crée ma première balise la baliseArticle, avec document.createElement() //
+let baliseArticle = document.createElement("article") ;
+// Je lui assigne sa classe 
+baliseArticle.className = "cart__item"; 
+// « data-id » ? (comment puis-je récupérer cette information ?)
+// « data-color » ? (comment puis-je récupérer cette information ?)
 
-                // Calculé le prix total et le nombre d'articles du panier
+// J’insère le nouvel enfant créé (donc baliseArticle appelée cart_item) dans mon élément parent (la section cart__items), grâce à la proprièté appendChild (ajout d’un enfant)
+baliseSection.appendChild(baliseArticle);
 
-                let totalQuantity = document.querySelector('#totalQuantity')
-                let totalPrice = document.querySelector('#totalPrice')
-                // On additionne la quantité de chaque produit pour trouver le nombre d'articles
-                cartTotalItems += parseInt(quantity)
-                // On multiplie pour chaque produit son prix par sa quantité, et on additionne
-                cartTotalPrice += productData.price * parseInt(quantity)
+// Je dois créer une div image à l’intérieur de laquelle se trouve une image
+let baliseDivImage = document.createElement("div") ;
+baliseDivImage.className = "cart__item__img";
 
-                totalQuantity.innerHTML = cartTotalItems
-                totalPrice.innerHTML = cartTotalPrice
+//Je crée ensuite ma balise image (qui sera l’enfant de ma BaliseDivImage)
+let baliseImage = document.createElement ("image") ;
+baliseImage.src= `${product.imageUrl}` ;
+balisemage.alt = `${product.altTxt}` ;
 
+// J’insère l’enfant créé (baliseImage) dans ma nouvelle div (baliseDivImage) avec appendChild
+baliseDivImage.appendChild(baliseImage) ;
 
-              })
+// Puis j’insère ma div image dans ma baliseArticle parent
+baliseArticle.appendChild(baliseDivImage);
 
+// Je crée une nouvelle div appelée cart__item__content
+let baliseDivContent = document.createElement ("div") ;
+baliseDivContent.className = "cart__item__content" ;
 
-          }
-        })
-    }
+// Je crée une nouvelle div appellée cart__item__content__description
+let baliseDivContentDescription = document.createElement("div") ;
+baliseDivContentDescription.className = "cart__item__content__description";
 
-  }
+// Dans ma div content description je vais créer : Un H2 et 2 P :
+let baliseTitleH2 = document.createElement("h2") ;
+// baliseTitleH2.innerText = `${product-name}` ; (Lequel mettre ?)
+// baliseTitleH2.TextContent = product.name ; (Lequel mettre ?)
+
+// J’insère ma baliseTitleH2 à sa balise parent Div content description
+baliseDivContentDescription.appendChild(baliseTitleH2)
+
+// Il me reste à insérer les 2 P :
+//Le premier P pour la couleur
+
+let baliseP1 = document.createElement ("p") ;
+// baliseP1.textContent = product.color ; (lequel mettre ?)
+// baliseP1.innerText = `${product-color}`, (lequel mettre ?)
+
+// Puis le second P pour le prix
+
+let baliseP2 = document.createElement ("p") ;
+// baliseP2.textContent = product.price  ; (Comment rajoute t-on le € ?) (lequel mettre?)
+// baliseP2.innerText = `${product-price}` ; (lequel mettre ?)
+
+// J’insère les deux P dans ma div content description
+baliseDivContentDescription.appendChild ( baliseP1) ;
+baliseDivContentDescription.appendChild ( baliseP2) ;
+
+// J’insère la div content description dans sa div parent div content avec appendChild
+baliseDivContent.appendChild(baliseDivContentDescription)
+
+// Je crée une nouvelle div appelée baliseDivContentSettings
+let baliseDivContentSettings = document.createElement ("div") ;
+baliseDivContentSettings.className = "cart__item__content__settings" ;
+
+// J’insère cette div dans sa div parent qui est baliseDivContent
+baliseDivContent.appendChild(baliseDivContentSettings)
+
+// Je crée une div pour la quantité
+let baliseDivContentSettingsQuantity = document.createElement ("div") ;
+baliseDivContentSettingsQuantity.className = "cart__item__settings__quantity";
+
+//J’insère cette div dans sa div parent 
+baliseDivContentSettings.appendChild(baliseDivContentSettingsQuantity) ;
+
+// Dans ma div baliseDivContentSettingsQuantity je dois insérer un P et un input 
+let balisePSettings = document.createElement("p") ;
+balisePSettings.textContent = "Qté : ";
+
+// J’insère mon P dans sa div parent
+baliseDivContentSettingsQuantity.appendChild(balisePSettings) ;
+
+// Je crée mon input
+let baliseInputSettings = document.createElement ("input") ;
+baliseInputSettings.className = "itemQuantity";
+ // (Comment ajouter les informations : type, class, min, max, value ?)
+
+// J’insère l’input dans sa div parent
+baliseDivContentSettingsQuantity.appendChild (baliseInputSettings) ;
+
+// Je crée une nouvelle div pour la suppression
+let baliseDivContentSettingsDelete = document.createElement("div") ;
+baliseDivContentSettingsDelete.className = "cart__item__settings__delete" ;
+
+// Je l’insère dans sa div parent
+baliseDivContentSettings.appendChild(baliseDivContentSettingsDelete) ;
+
+// Je dois insérer un P dans ma balise delete
+let balisePDelete = document.createElement ("p") ;
+balisePDelete.className = "deleteItem" ;
+balisePDelete.textContent = « Supprimer » ;
+
+// J’insère mon p dans sa balise parent
+baliseDivContentSettingsDelete.appendChild(balisePDelete) ;
+
+// J’insère la div content à sa div parent article
+baliseArticle.appendChild(baliseDivContent);
+
 }
 
-// getProductOfCart()
+
+
+
+
+
+
+
+
+
+  }
+
+}
+
+
+
+
+
+// Je récupère les
+/*
+///// Formulaire de Validation : RegExp et POST request/////
+
+// Création du formulaire//
+
+let form = document.querySelector('.cart__order__form');
+
+console.log(form.firstName);
+
+// Ecoute de la modification du prénom
+
+form.firstName.addEventListener('change',function(){
+  validFirstname(this);
+})
+
+const validFirstname = function (inputFirstname){
+  let  firstNameRegExp = new RegExp(
+    '^[a-zA-Z-]+[a-zA-Z]{2,20}$', 'g'
+    )
+}; */
+
--- front/js/product.js
+++ front/js/product.js
@@ -2,23 +2,22 @@
 
 let productChoices = null;
 
-// 1. Création du panier dans le LS 
+// 1. Je vais créer le panier dans le LocalStorage
 
-let basket = localStorage.getItem("products");
+let basket = JSON.parse(localStorage.getItem("products"));
+
 if (basket == null) {
     basket = [];
 }
 
 
-//// 1 : Récupérer de l'id dynamique de l'url : 
+//// 2 : Je récupére l'id dynamique de l'url :
 
-let productId = new URL(window.location.href).searchParams.get("id"); // --> On récupére l'id avec les paramétres de l'url puis on récupère l'URL de la page active et on extrait l'ID d'un produit //
+// Avec new URL je vais récupèrer l'URL de la page active puis Avec searchParams.get je vais récupérer l'id du produit
 
+let productId = new URL(window.location.href).searchParams.get("id");
 
-console.log(productId);
-
-
-// 2 : Récupérer les élements pour injecter les infos du produit
+//// 3 : Je récupére les élements pour injecter les infos du produit
 
 let productTitle = document.getElementById("title");
 let productColor = document.getElementById("colors");
@@ -27,8 +26,7 @@ let productDescription = document.getElementById("description");
 let productPrice = document.getElementById("price");
 
 
-
-// 3 : Appeler l'API pour récupérer les informations du produit à afficher
+//// 4 : Je vais appeler l'API pour récupérer les informations du produit à afficher
 
 fetch("http://localhost:3000/api/products/" + productId)
     .then((response) => {
@@ -58,76 +56,93 @@ fetch("http://localhost:3000/api/products/" + productId)
     })
 
 
-// 4 : Ajout du produit et ses infos au localstorage : "Ajouter un produit au panier"
 
+//// 5: A l'évènement 'click' sur le bouton 'btnAddProductToBasket' --> Cela envoit les produits au localStorage
 
+            // 1. Je crée une variable pour le bouton "ajouter au panier", qui permet de sélectionner l'id du bouton ('addToCart') grâce à querySelector
 
-// Lorsqu'on clique sur le bouton 'btnAddProductToBasket' --> On envoie les produits au localStorage
+            let btnAddProductToBasket = document.querySelector("#addToCart");
 
-let btnAddProductToBasket = document.querySelector("#addToCart");
+            // 2. Je crée une fonction qui au clic, va récupérer la couleur choisie & la quantité saisie
 
+            btnAddProductToBasket.addEventListener('click', () => {
 
-btnAddProductToBasket.addEventListener('click', () => {
+            // 3. Je récupére la couleur sélectionnée
+            let colorOfProduct = document.querySelector("#colors").value;
 
-    // 1. Récupération de la couleur sélectionnée
-    let colorOfProduct = document.querySelector("#colors").value;
+            // 4. Je récupére la quantité saisie
 
-    // 2. Récupération de la quantité saisie
+            let quantityOfProduct = document.querySelector("#quantity").value;
 
-    let quantityOfProduct = document.querySelector("#quantity").value;
+            // 5. Conditions du panier
 
-    // 3. Conditions du panier
-
-    if (!colorOfProduct) {
-        alert("Veuillez choisir une couleur")
-    } else if (quantityOfProduct <= 0) {
-        alert("Vous devez ajouter au moins un produit par commande")
-    } else if (quantityOfProduct > 100) {
-        alert("Vous ne pouvez pas ajouter plus de 100 produits par commande")
-
-    } else {
-
-        // Création de l'objet à envoyer au LocalStorage
-        productChoices = {
-            id: productId,
-            color: colorOfProduct,
-            quantity: quantityOfProduct,
-        }
-
-
-        console.log(productChoices);
-        console.log(basket);
-
-
-        // Si j'ai déjà des produits dans mon panier : je lance la recherche pour pouvoir le faire le cumul des quantités
-
-        if (basket) {
-            let found = basket.find(
-                (element) => element.id == productId && element.color == colorOfProduct
-            );
-
-            if (found != undefined) {
-
-                // 6. Valeur LS + value actuelle : transforme les chaines de caractères en nombre
-                let totalQuantity = parseInt(found.quantityOfProduct) + parseInt(quantityOfProduct);
-                found.quantityOfProduct = totalQuantity;
+            if (!colorOfProduct) {
+                alert("Veuillez choisir une couleur")
+            } else if (quantityOfProduct <= 0) {
+                alert("Vous devez ajouter au moins un produit par commande")
+            } else if (quantityOfProduct > 100) {
+                alert("Vous ne pouvez pas ajouter plus de 100 produits par commande")
 
             } else {
-                // 7. Enregistrer les éléments dans le LS s'il n'existe pas
+
+                // 6. Création de l'objet à envoyer au LocalStorage
+                productChoices = {
+                    id: productId,
+                    color: colorOfProduct,
+                    quantity: quantityOfProduct,
+                }
+
+
+                console.log(productChoices);
+
+                console.log(basket);
+
+            // 7. Si j'ai déjà des produits dans mon panier : je lance la recherche pour pouvoir le faire le cumul des quantités
+
+            if (basket != null) {
+
+                let found = basket.findIndex(
+                    (element) => element.id === productId && element.color == colorOfProduct
+                );
+
+                if (found !== -1) {
+
+                    // 8. Je crée une variable qui additionne deux fonctions parseInt qui vont convertir la chaine de caractères en nombre.
+                    let totalQuantity = parseInt(basket[found].quantity) + parseInt(quantityOfProduct);
+
+                    basket[found].quantity = totalQuantity;
+
+                    // 9. J'ajoute l'article
+
+                    basket.push(productChoices);
+
+                    // 10. Je supprime le dernier article
+
+                    basket.pop(); 
+
+                } else {
+
+                    // 11. J'enregistre les éléments dans le LocalStorage s'il n'existe pas
+                    basket.push(productChoices);
+                }
+
+            } else {
                 basket.push(productChoices);
             }
 
-        } else {
-            basket.push(productChoices);
+
+            // 12. J'enregistre le nouvel élement (proprièté localStorage.setItem) et j'additionne dans le LocalStorage de ma page product.js sous forme de chaine de caractères JSON, grâce à JSONStringify.
+
+            localStorage.setItem("products", JSON.stringify(basket));
+
+            // 13. Je récupère l'URL pour qu'au clic l'utilisateur soit basculé sur la page cart.html
+
+            document.location.href = "cart.html";
+
         }
 
-
-        // 8. Enregistrer le nouvel élement et on additionne dans le LS/STRINGIFY = On récupère sous forme de chaine de caractère (string)
-
-        localStorage.setItem("products", JSON.stringify(basket));
-
-    }
 });
+
 
 
 
