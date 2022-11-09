5af1c08c3f73a436b7a119471bfaab2c0d41b0e1
No0910 <nestea_93@hotmail.fr>
2022-11-09 18:20:57 +0100

    Fonctions en cours (total Quantité, total Prix, modifier, supprimer)

--- front/js/cart.js
+++ front/js/cart.js
@@ -26,11 +26,10 @@ document.getElementById('totalQuantity').innerHTML = nbProducts;
 
 let product = basket[i];
 
-console.log( product );
+console.log(product);
 
 // 3. Pour chaque produit, je dois créer une ligne dans mon tableau ( qui correspond à ce qui est commenté dans cart.html )
 
-// TODO
 
 // Je veux créer un article, à l’intérieur de ma section déjà existante
 // Je récupère l’élément « section » qui existe déjà pour y créer mes éléments actuellement commentés dans mon html
@@ -40,19 +39,21 @@ let baliseSection = document.getElementById ("cart__items") ;
 // Puis j'appelle l'API avec la fonction fetch
 
 //Puis appeller l'API //
-fetch("http://localhost:3000/api/products/" + basket.id)
+fetch("http://localhost:3000/api/products/" + product.id) //product.id récupère l'identifiant du produit dans le tableau crée ligne 27, .id est la clé
     .then((response) => {
         if (response.ok) {
 
             response.json()
-                .then((baskets) => {
-                    for (let basket of nbProducts) {
+                .then((monProduit) => {
+
+                  console.log(monProduit);
+                     
 
                       // Je crée ma première balise la baliseArticle
                       let baliseArticle = document.createElement("article") ;
                       baliseArticle.className = "cart__item";
-                      baliseArticle.setAttribute("data-id");
-                      baliseArticle.setAttribute("data-color");
+                      baliseArticle.setAttribute("data-id", monProduit._id );
+                      baliseArticle.setAttribute("data-color", product.color);
 
 
                       // Je dois créer une div image à l’intérieur de laquelle se trouve une image
@@ -60,8 +61,9 @@ fetch("http://localhost:3000/api/products/" + basket.id)
                       baliseDivImage.classList.add("cart__item__img");
 
                       //Je crée ensuite ma balise image (qui sera l’enfant de ma BaliseDivImage)
-                      let baliseImage = document.createElement ("image") ;
-                      baliseImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+                      let baliseImage = document.createElement ("img") ;
+                      baliseImage.src = `${monProduit.imageUrl}`;
+                      baliseImage.alt = `${monProduit.altTxt}`;
 
                       // Je crée une nouvelle div appelée cart__item__content
                       let baliseDivContent = document.createElement ("div") ;
@@ -73,9 +75,8 @@ fetch("http://localhost:3000/api/products/" + basket.id)
 
                       // Dans ma div content description je vais créer : Un H2 et 2 P :
                       let baliseTitleH2 = document.createElement("h2") ;
-                      baliseTitleH2.innerText = product.name;
+                      baliseTitleH2.innerText = monProduit.name;
                      
-
                       // Il me reste à insérer les 2 P :
                       //Le premier P pour la couleur
 
@@ -86,7 +87,7 @@ fetch("http://localhost:3000/api/products/" + basket.id)
                       // Puis le second P pour le prix
 
                       let baliseP2 = document.createElement ("p") ;
-                      baliseP2.textContent = product.price + " €" ;
+                      baliseP2.textContent = monProduit.price + " €" ;
                      
 
                       // Je crée une nouvelle div appelée baliseDivContentSettings
@@ -104,13 +105,13 @@ fetch("http://localhost:3000/api/products/" + basket.id)
                       
                       // Je crée mon input
                       let baliseInputSettings = document.createElement ("input") ;
-                      baliseInputSettings.className = "itemQuantity";
+                      baliseInputSettings.classList.add("itemQuantity");
                       baliseInputSettings.setAttribute("type","number");
                       baliseInputSettings.setAttribute("class","itemQuantity");
                       baliseInputSettings.setAttribute("name","itemQuantity");
                       baliseInputSettings.setAttribute("min","1");
                       baliseInputSettings.setAttribute("max","100");
-                      baliseInputSettings.setAttribute("value","42");
+                      baliseInputSettings.setAttribute("value", product.quantity); 
 
                       // Je crée une nouvelle div pour la suppression
                       let baliseDivContentSettingsDelete = document.createElement("div") ;
@@ -137,13 +138,80 @@ fetch("http://localhost:3000/api/products/" + basket.id)
                       baliseDivContentSettingsQuantity.appendChild (baliseInputSettings);
                       baliseDivContentSettings.appendChild(baliseDivContentSettingsDelete);
                       baliseDivContentSettingsDelete.appendChild(balisePDelete);
-                   
+
+                      //TODO calculer les quantités commandées de ma variable product
+                      //TODO calculer le prix total des produits commandés monProduit/product mix 
+                      //TODO écrire la fonction de suppression d'un produit 
+                      //TODO fonction qui recalcule le panier quand je change les quantités
+
 
                     }
 
-                })
+                )
         }
       }
    )
 
     }}
+
+    // Fonction qui calcule le total des quantités de produits du panier
+
+      function calculateTotalQuantity() {
+      let getTotalQuantity = document.querySelector("#totalQuantity");
+      let basket = JSON.parse(localStorage.getItem("products"));
+      let calculateTotalQuantity= [];
+      console.log(calculateTotalQuantity)
+
+      let totalQuantity = 0;
+      for( let product of basket ){
+          totalQuantity += parseInt(product.quantity);
+      }
+
+      calculateTotalQuantity.push(totalQuantity);
+      getTotalQuantity.innerText = totalQuantity;
+
+      }
+
+    calculateTotalQuantity();
+
+      
+   // Fonction qui calcule le prix total du panier
+
+    function calculateTotalPrice () {
+
+    let basket = JSON.parse(localStorage.getItem("products"));
+    let totalPrice = document.querySelector("#totalPrice");
+    let total = basket.reduce((total, product) => total + product.price * product.quantity , 0)
+     
+     for (let i = 0; i < basket.length; i++) {
+
+    let totalPrice = 0;
+
+     
+    };
+
+      totalPrice.innerHTML = total;
+    }
+
+   calculateTotalPrice(); 
+
+
+  // Fonction lorsqu'on modifie une quantité, le panier se met à jour
+
+  function updatePriceAndQuantity (id,newValue) {
+  
+    const productToUpdate = basket.find(product => product.id === id);
+    productToUpdate.quantity = Number(newValue);
+
+    console.log("productToUpdate",productToUpdate);
+    console.log("newValue",newValue);
+
+  } 
+
+
+  
+ // Fonction de suppression d'un produit
+    
+function deleteProduct (){
+  let deleteButton = document.querySelectorAll(".cart__item .deleteItem");
+}
